0566bf3449d2d93b512d5d4ef7026e8095620e5d
omkar chawan <omkarchawan93@gmail.com>
2025-11-28 09:25:46 +0530

    First commit

--- login/firebase-config.js
+++ login/firebase-config.js
@@ -1,7 +1,4 @@
-//import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBxjRL6SZjvI8Ty5ecNNYxtpxrFVUWJQAM",
   authDomain: "fir-login-bf621.firebaseapp.com",
@@ -12,21 +9,4 @@ const firebaseConfig = {
   measurementId: "G-YQ3SPSDW5T"
 };
 
-
-/*
-const firebaseConfig = {
-  apiKey: "AIzaSyBLmlRbxZI-6ONXS4PEUFMXCLUD-drBCTY",
-  authDomain: "art-contest-2025.firebaseapp.com",
-  projectId: "art-contest-2025",
-  storageBucket: "art-contest-2025.firebasestorage.app",
-  messagingSenderId: "523292533996",
-  appId: "1:523292533996:web:4ef1909f548458b6c1f1c9",
-  measurementId: "G-FNW5G94341"
-};
-
-*/
-
-
-//const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 firebase.initializeApp(firebaseConfig);
